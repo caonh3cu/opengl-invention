4384250b5699b5a564c2cc3199fd9dc64a049d3d
密涅瓦的猫 <caoaj2113@mails.jlu.edu.cn>
2018-08-22 14:42:34 +0800

    Update object.cpp

--- VREngine/VREngine/object/object.cpp
+++ VREngine/VREngine/object/object.cpp
@@ -23,8 +23,12 @@ void ObjectManager::readConfig(string config) {
 	}
 }
 void ObjectManager::drawObject(string name,GLuint shader) {
+	map<string,Object>::iterator iter = objects.find(name);
+	if (iter == objects.end())
+		return;
+	Object to = iter->second;
+
 	vector<Mesh*> &meshes = AssetManager::getInstance()->meshes;
-	Object to = objects[name];
 	glUniformMatrix4fv(glGetUniformLocation(shader, "model"), 1, GL_FALSE, &(to.getModelMat())[0][0]);
 	for (int j = to.meshIndex.first; j < to.meshIndex.second; j++) {
 		meshes[j]->meterial->bindMeterial(shader);
